2fd1a436f3118cc6d4f0205b67977730e5a0d9b3
Reil <francis@reilaos.com>
2012-07-07 00:37:04 -0600

    Updated RemoveEntity to also have the ability to create an explosion where the removed entity used to be.

--- src/com/reilaos/bukkit/TheThuum/delays/RemoveEntity.java
+++ src/com/reilaos/bukkit/TheThuum/delays/RemoveEntity.java
@@ -5,13 +5,22 @@ import org.bukkit.entity.Entity;
 public class RemoveEntity implements Runnable {
 	
 	Entity removeMe;
+	boolean explode;
 	
-	public RemoveEntity(Entity Victim){
-		removeMe = Victim;
+	public RemoveEntity(Entity victim){
+		this.removeMe = victim;
+		this.explode  = false;
+	}
+	
+	public RemoveEntity(Entity victim, boolean explode){
+		this.removeMe = victim;
+		this.explode  = explode;
 	}
 	
 	@Override
 	public void run() {
-		if (removeMe != null && !removeMe.isDead()) removeMe.remove();			
+		if (removeMe == null) return;
+		if (explode  == true) removeMe.getWorld().createExplosion(removeMe.getLocation(), 0);
+		if (!removeMe.isDead()) removeMe.remove();
 	}
 }
