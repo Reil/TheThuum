0552f2c5127d784ce5928ddf89a4f2ce0419061a
Reil <francis@reilaos.com>
2012-07-07 00:12:38 -0600

    - Adds command capabilities for shouts

--- src/com/reilaos/bukkit/TheThuum/GreyBeard.java
+++ src/com/reilaos/bukkit/TheThuum/GreyBeard.java
@@ -12,7 +12,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChatEvent;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.ChatColor;
 
 import com.reilaos.bukkit.TheThuum.shouts.ShoutType;
 
@@ -33,6 +35,7 @@ public class GreyBeard implements Listener{
 			for(power = 0; power < length; power++){
 				if(!message[power].equalsIgnoreCase(shoutType.shout.words()[power])) continue shouting;
 			}
+			
 			switch(Plugin.thisOne.getConfig().getInt("display.audible chat")){
 			case (2):
 				event.setMessage(ChatColor.valueOf(Plugin.thisOne.getConfig().getString("display.color").toUpperCase()) + event.getMessage());
@@ -43,8 +46,48 @@ public class GreyBeard implements Listener{
 				event.setCancelled(true);
 			break;
 			}
+			
 			shout(event.getPlayer(), shoutType, power);
 			return;
+		}
+	}
+	
+	@EventHandler
+	public void onPlayerCommand(PlayerCommandPreprocessEvent event){
+		Player dovahkiin = event.getPlayer();
+		String[] message = event.getMessage().split(" ");
+		message[0] = message[0].substring(1);
+		if (message.length > 2) return;
+		shouting: for (ShoutType shoutType : ShoutType.values()){
+			int power = 1;
+			if (!message[0].equalsIgnoreCase(shoutType.shout.words()[0])) continue shouting;
+			event.setCancelled(true);
+			
+			try {
+				if(message.length == 2) power = Integer.parseInt(message[1]);
+			}
+			catch (NumberFormatException e){
+				dovahkiin.sendMessage("Invalid parameter! Format is: /SHOUTNAME [power]");
+				return;
+			}
+			if (power < 0 || power > 3) {
+				dovahkiin.sendMessage("Invalid power! Must be 1, 2 or 3.");
+				return;
+			}
+			
+			int audible = Plugin.thisOne.getConfig().getInt("display.audible command");
+			if (audible > 0) {
+				StringBuilder say = new StringBuilder(ChatColor.valueOf(Plugin.thisOne.getConfig().getString("display.color").toUpperCase()).toString());
+				for(int i = 0; i < power; i++){
+					say.append(shoutType.shout.words()[i].toUpperCase()).append(" ");
+				}
+				say.insert(say.length()-1, '!');
+				
+				if      (audible == 1) dovahkiin.sendMessage(say.toString());
+				else if (audible == 2) dovahkiin.chat(say.toString());
+			}
+		
+			shout(event.getPlayer(), shoutType, power);
 		}
 	}
 	
@@ -70,7 +113,7 @@ public class GreyBeard implements Listener{
 	// Good practice would put this as its own player listener, but still.
 	
 	Configuration shoutCooldowns;	
-	Hashtable<Player, Set<ShoutType>> onCooldown = new Hashtable<Player, Set<ShoutType>>();
+	Hashtable<String, Set<ShoutType>> onCooldown = new Hashtable<String, Set<ShoutType>>();
 	
 	@EventHandler
 	public void onPlayerQuit(PlayerQuitEvent event){
@@ -85,10 +128,10 @@ public class GreyBeard implements Listener{
 		
 		if (Plugin.thisOne.getConfig().getBoolean("single cooldown", true)) shout = ShoutType.FUSRODAH;
 		
-		if (!onCooldown.containsKey(dovahkiin)) onCooldown.put(dovahkiin, EnumSet.noneOf(ShoutType.class));
-		if (onCooldown.get(dovahkiin).contains(shout)) return false;
+		if (!onCooldown.containsKey(dovahkiin.getName())) onCooldown.put(dovahkiin.getName(), EnumSet.noneOf(ShoutType.class));
+		if (onCooldown.get(dovahkiin.getName()).contains(shout)) return false;
 
-		onCooldown.get(dovahkiin).add(shout);
+		onCooldown.get(dovahkiin.getName()).add(shout);
 		Cooldown task = new Cooldown(dovahkiin, shout);
 		Plugin.scheduler.scheduleSyncDelayedTask(Plugin.thisOne, task,cooldownDuration);
 		return true;
@@ -105,8 +148,8 @@ public class GreyBeard implements Listener{
 		
 		@Override
 		public void run() {
-			if (!onCooldown.containsKey(dovahkiin)) return;
-			onCooldown.get(dovahkiin).remove(shout);
+			if (!onCooldown.containsKey(dovahkiin.getName())) return;
+			onCooldown.get(dovahkiin.getName()).remove(shout);
 			dovahkiin.sendMessage(Plugin.thisOne.getConfig().getString("cooldown.ready message"));
 		}
 	}
@@ -120,7 +163,7 @@ public class GreyBeard implements Listener{
 		
 		@Override
 		public void run() {
-			if (!dovahkiin.isOnline()) onCooldown.remove(dovahkiin);
+			if (!dovahkiin.isOnline()) onCooldown.remove(dovahkiin.getName());
 		}
 	}
 }
